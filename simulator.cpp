3706691f9defe12b4c873fcccc93c09f12d6713c
wolfee <wolfee.dm@gmail.com>
2021-10-30 16:15:01 +0200

    simulator handles ticks and powerup pickups

---
+++ simulator.cpp
@@ -0,0 +1,137 @@
+#include "simulator.h"
+#include <stdexcept>
+
+Simulator::Simulator(const GameDescription& gameDescription)
+    : mGameDescription(gameDescription)
+{
+}
+
+void Simulator::SetState(const TickDescription& state)
+{
+    mState = state;
+}
+
+void Simulator::SetVampireMove(int id, const Answer& move)
+{
+    mVampireMoves[id] = move;
+}
+
+TickDescription Simulator::Tick()
+{
+    if (mState.mRequest.mTick == -1) {
+        throw std::runtime_error("Calling tick without setting state!");
+    }
+
+    if (const auto it = mVampireMoves.find(mState.mMe.mId); it != mVampireMoves.end()) {
+        if (it->second.mSteps.size() > 2 && mState.mMe.mRunningShoesTick <= 0) {
+            throw std::runtime_error("Stepping more than 2 but there are no running shoes!");
+        }
+    }
+
+    for (const auto& element : mState.mEnemyVampires) {
+        if (const auto it = mVampireMoves.find(mState.mMe.mId); it != mVampireMoves.end()) {
+            if (it->second.mSteps.size() > 2 && element.mRunningShoesTick <= 0) {
+                throw std::runtime_error("Stepping more than 2 but there are no running shoes!");
+            }
+        }
+    }
+
+    TickDescription retVal = mState;
+
+    // Rule:
+    // a) powerup show up - not simulated, comes as state
+    // b) powerup pick up
+    // c) blow up grenades recursively
+    // d) calculate grenade damage
+    // e) calculate light damage - NOT SIMULATED! (maybe later. i didn't understand the rule.)
+    // f) plant grenades
+    // g) move
+
+    // Implementation:
+    // 1) recalculate ticks
+    RecalculateTicks(retVal);
+    // 2) remove disappeared powerups
+    RemoveDisappearedPowerups(retVal);
+    // 3) (b) powerup pick up
+    PowerupPickUp(retVal);
+
+    mState = TickDescription();
+    mVampireMoves.clear();
+
+    return retVal;
+}
+
+void Simulator::RecalculateTicks(TickDescription& state)
+{
+    state.mRequest.mTick++;
+
+    for (auto& grenade : state.mGrenades) {
+        grenade.mTick--;
+    }
+
+    for (auto& powerup : state.mPowerUps) {
+        if (powerup.mRemainingTick < -1) {
+            powerup.mRemainingTick++;
+        } else if (powerup.mRemainingTick == -1) {
+            powerup.mRemainingTick = 10;
+        } else {
+            powerup.mRemainingTick--;
+        }
+    }
+
+    state.mMe.mRunningShoesTick = std::max(state.mMe.mRunningShoesTick - 1, 0);
+    for (auto& vampire : state.mEnemyVampires) {
+        vampire.mRunningShoesTick = std::max(vampire.mRunningShoesTick - 1, 0);
+    }
+}
+
+void Simulator::RemoveDisappearedPowerups(TickDescription& state)
+{
+    state.mPowerUps.erase(
+        std::remove_if(state.mPowerUps.begin(), state.mPowerUps.end(), [](const auto& element) { return element.mRemainingTick == 0; }), state.mPowerUps.end());
+}
+
+void Simulator::PowerupPickUp(TickDescription& state)
+{
+    std::vector<Vampire*> vampRefs;
+    vampRefs.push_back(&state.mMe);
+    for (auto& element : state.mEnemyVampires) {
+        vampRefs.push_back(&element);
+    }
+
+    std::vector<PowerUp> survivors;
+
+    for (const auto& pu : state.mPowerUps) {
+        bool shouldDelete = false;
+        for (auto* vampire : vampRefs) {
+            if (vampire->mX == pu.mX && vampire->mY == pu.mY) {
+                shouldDelete = true;
+                switch (pu.mType) {
+                case PowerUp::Type::Battery: {
+                    vampire->mGrenadeRange++;
+                    break;
+                }
+                case PowerUp::Type::Grenade: {
+                    vampire->mPlacableGrenades++;
+                    break;
+                }
+                case PowerUp::Type::Shoe: {
+                    vampire->mRunningShoesTick += mGameDescription.mMapSize * 2;
+                    break;
+                }
+                case PowerUp::Type::Tomato: {
+                    vampire->mHealth = std::min(3, vampire->mHealth + 1);
+                    break;
+                }
+                default:
+                    throw std::runtime_error("Unhandled powerup type!");
+                }
+            }
+        }
+        if (!shouldDelete) {
+            survivors.push_back(pu);
+        }
+    }
+
+    state.mPowerUps = survivors;
+}
---
+++ test/test_simulator.cpp
@@ -0,0 +1,292 @@
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+#include <memory>
+
+#include "../simulator.h"
+#include "../solver.h"
+
+class SimulateTest : public testing::Test {
+public:
+    SimulateTest()
+    {
+        std::vector<std::string> startInfo = {
+            "MESSAGE OK",
+            "LEVEL 1",
+            "GAMEID 775",
+            "TEST 1",
+            "MAXTICK 500",
+            "GRENADERADIUS 2",
+            "SIZE 11",
+        };
+        mGameDescripton = solver::parseGameDescription(startInfo);
+
+        mSimulator = std::make_unique<Simulator>(mGameDescripton);
+    }
+
+protected:
+    GameDescription mGameDescripton;
+    std::unique_ptr<Simulator> mSimulator;
+};
+
+TEST_F(SimulateTest, TickNoEffect)
+{
+    // clang-format off
+    std::vector<std::string> info = {
+        "REQ 775 0 1",
+        "VAMPIRE 1 1 1 3 1 2 0",
+        "VAMPIRE 3 9 9 3 1 2 0",
+        "VAMPIRE 4 1 9 3 1 2 0",
+        "VAMPIRE 2 9 1 3 1 2 0",
+        "BAT1 4 1 5 1 6 1 3 2 7 2 2 3 3 3 7 3 8 3 1 4 9 4 1 5 9 5 1 6 9 6 2 7 3 7 7 7 8 7 3 8 7 8 4 9 5 9 6 9",
+        "BAT2 5 2 4 3 6 3 3 4 7 4 2 5 8 5 3 6 7 6 4 7 6 7 5 8",
+        "BAT3 5 3 5 4 3 5 4 5 5 5 6 5 7 5 5 6 5 7",
+    };
+    // clang-format on
+    TickDescription state = solver::parseTickDescription(info);
+    mSimulator->SetState(state);
+    const TickDescription newState = mSimulator->Tick();
+    state.mRequest.mTick++;
+    ASSERT_EQ(state, newState);
+}
+
+TEST_F(SimulateTest, TickDecreases)
+{
+    // clang-format off
+    std::vector<std::string> info = {
+        "REQ 775 0 1",
+        "GRENADE 1 1 3 5 2",
+        "GRENADE 1 9 3 3 2",
+        "POWERUP TOMATO -3 2 1",
+        "POWERUP TOMATO -1 2 1",
+        "POWERUP GRENADE 2 3 1",
+        "POWERUP BATTERY 10 7 1",
+        "POWERUP SHOE -10 8 1"
+    };
+    // clang-format on
+    const TickDescription tick = solver::parseTickDescription(info);
+    mSimulator->SetState(tick);
+    const TickDescription newState = mSimulator->Tick();
+    ASSERT_EQ(newState.mGrenades[0].mTick, 4);
+    ASSERT_EQ(newState.mGrenades[1].mTick, 2);
+    ASSERT_EQ(newState.mPowerUps[0].mRemainingTick, -2);
+    ASSERT_EQ(newState.mPowerUps[1].mRemainingTick, 10);
+    ASSERT_EQ(newState.mPowerUps[2].mRemainingTick, 1);
+    ASSERT_EQ(newState.mPowerUps[3].mRemainingTick, 9);
+    ASSERT_EQ(newState.mPowerUps[4].mRemainingTick, -9);
+}
+
+TEST_F(SimulateTest, PowerupDisappear)
+{
+    // clang-format off
+    std::vector<std::string> info = {
+        "REQ 775 0 1",
+        "POWERUP TOMATO -3 2 1",
+        "POWERUP TOMATO -1 2 1",
+        "POWERUP TOMATO 1 2 1",
+    };
+    // clang-format on
+    const TickDescription tick = solver::parseTickDescription(info);
+    mSimulator->SetState(tick);
+    const TickDescription newState = mSimulator->Tick();
+    ASSERT_EQ(newState.mPowerUps.size(), 2);
+    ASSERT_EQ(newState.mPowerUps[0].mRemainingTick, -2);
+    ASSERT_EQ(newState.mPowerUps[1].mRemainingTick, 10);
+}
+
+TEST_F(SimulateTest, PowerupPickupSingleMeTomatoNoEffect)
+{
+    // clang-format off
+    std::vector<std::string> info = {
+        "REQ 775 0 1",
+        "VAMPIRE 1 1 1 3 1 2 0",
+        "POWERUP TOMATO 3 2 1",
+        "POWERUP TOMATO 3 1 1",
+        "POWERUP TOMATO 5 3 1",
+    };
+    // clang-format on
+    const TickDescription tick = solver::parseTickDescription(info);
+    mSimulator->SetState(tick);
+    const TickDescription newState = mSimulator->Tick();
+    ASSERT_EQ(newState.mPowerUps.size(), 2);
+    ASSERT_EQ(newState.mPowerUps[0].mRemainingTick, 2);
+    ASSERT_EQ(newState.mPowerUps[1].mRemainingTick, 4);
+    ASSERT_EQ(newState.mMe.mHealth, 3);
+}
+
+TEST_F(SimulateTest, PowerupPickupSingleMeTomato)
+{
+    // clang-format off
+    std::vector<std::string> info = {
+        "REQ 775 0 1",
+        "VAMPIRE 1 1 1 1 1 2 0",
+        "POWERUP TOMATO 3 2 1",
+        "POWERUP TOMATO 3 1 1",
+        "POWERUP TOMATO 5 3 1",
+    };
+    // clang-format on
+    const TickDescription tick = solver::parseTickDescription(info);
+    mSimulator->SetState(tick);
+    const TickDescription newState = mSimulator->Tick();
+    ASSERT_EQ(newState.mPowerUps.size(), 2);
+    ASSERT_EQ(newState.mPowerUps[0].mRemainingTick, 2);
+    ASSERT_EQ(newState.mPowerUps[1].mRemainingTick, 4);
+    ASSERT_EQ(newState.mMe.mHealth, 2);
+}
+
+TEST_F(SimulateTest, PowerupPickupMeWithOtherTomato)
+{
+    // clang-format off
+    std::vector<std::string> info = {
+        "REQ 775 0 1",
+        "VAMPIRE 1 1 1 1 1 2 0",
+        "VAMPIRE 2 1 1 2 1 2 0",
+        "POWERUP TOMATO 3 2 1",
+        "POWERUP TOMATO 3 1 1",
+        "POWERUP TOMATO 5 3 1",
+    };
+    // clang-format on
+    const TickDescription tick = solver::parseTickDescription(info);
+    mSimulator->SetState(tick);
+    const TickDescription newState = mSimulator->Tick();
+    ASSERT_EQ(newState.mPowerUps.size(), 2);
+    ASSERT_EQ(newState.mPowerUps[0].mRemainingTick, 2);
+    ASSERT_EQ(newState.mPowerUps[1].mRemainingTick, 4);
+    ASSERT_EQ(newState.mMe.mHealth, 2);
+    ASSERT_EQ(newState.mEnemyVampires[0].mHealth, 3);
+}
+
+TEST_F(SimulateTest, PowerupPickupOthersTomato)
+{
+    // clang-format off
+    std::vector<std::string> info = {
+        "REQ 775 0 1",
+        "VAMPIRE 1 4 1 1 1 2 0",
+        "VAMPIRE 2 1 1 2 1 2 0",
+        "VAMPIRE 3 1 1 1 1 2 0",
+        "POWERUP TOMATO 3 2 1",
+        "POWERUP TOMATO 3 1 1",
+        "POWERUP TOMATO 5 3 1",
+    };
+    // clang-format on
+    const TickDescription tick = solver::parseTickDescription(info);
+    mSimulator->SetState(tick);
+    const TickDescription newState = mSimulator->Tick();
+    ASSERT_EQ(newState.mPowerUps.size(), 2);
+    ASSERT_EQ(newState.mPowerUps[0].mRemainingTick, 2);
+    ASSERT_EQ(newState.mPowerUps[1].mRemainingTick, 4);
+    ASSERT_EQ(newState.mMe.mHealth, 1);
+    ASSERT_EQ(newState.mEnemyVampires[0].mHealth, 3);
+    ASSERT_EQ(newState.mEnemyVampires[1].mHealth, 2);
+}
+
+TEST_F(SimulateTest, PowerupPickupOthersTomato2)
+{
+    // clang-format off
+    std::vector<std::string> info = {
+        "REQ 775 0 1",
+        "VAMPIRE 1 4 1 1 1 2 0",
+        "VAMPIRE 2 1 1 2 1 2 0",
+        "VAMPIRE 3 1 1 3 1 2 0",
+        "POWERUP TOMATO 3 2 1",
+        "POWERUP TOMATO 3 1 1",
+        "POWERUP TOMATO 5 3 1",
+    };
+    // clang-format on
+    const TickDescription tick = solver::parseTickDescription(info);
+    mSimulator->SetState(tick);
+    const TickDescription newState = mSimulator->Tick();
+    ASSERT_EQ(newState.mPowerUps.size(), 2);
+    ASSERT_EQ(newState.mPowerUps[0].mRemainingTick, 2);
+    ASSERT_EQ(newState.mPowerUps[1].mRemainingTick, 4);
+    ASSERT_EQ(newState.mMe.mHealth, 1);
+    ASSERT_EQ(newState.mEnemyVampires[0].mHealth, 3);
+    ASSERT_EQ(newState.mEnemyVampires[1].mHealth, 3);
+}
+
+TEST_F(SimulateTest, PowerupPickupGrenade)
+{
+    // clang-format off
+    std::vector<std::string> info = {
+        "REQ 775 0 1",
+        "VAMPIRE 1 1 1 1 1 2 0",
+        "POWERUP GRENADE 3 1 1",
+    };
+    // clang-format on
+    const TickDescription tick = solver::parseTickDescription(info);
+    mSimulator->SetState(tick);
+    const TickDescription newState = mSimulator->Tick();
+    ASSERT_EQ(newState.mPowerUps.size(), 0);
+    ASSERT_EQ(newState.mMe.mPlacableGrenades, 2);
+}
+
+TEST_F(SimulateTest, PowerupPickupBattery)
+{
+    // clang-format off
+    std::vector<std::string> info = {
+        "REQ 775 0 1",
+        "VAMPIRE 1 1 1 1 1 2 0",
+        "POWERUP BATTERY 3 1 1",
+    };
+    // clang-format on
+    const TickDescription tick = solver::parseTickDescription(info);
+    mSimulator->SetState(tick);
+    const TickDescription newState = mSimulator->Tick();
+    ASSERT_EQ(newState.mPowerUps.size(), 0);
+    ASSERT_EQ(newState.mMe.mGrenadeRange, 3);
+}
+
+TEST_F(SimulateTest, PowerupPickupShoe)
+{
+    // clang-format off
+    std::vector<std::string> info = {
+        "REQ 775 0 1",
+        "VAMPIRE 1 1 1 1 1 2 0",
+        "POWERUP SHOE 3 1 1",
+    };
+    // clang-format on
+    const TickDescription tick = solver::parseTickDescription(info);
+    mSimulator->SetState(tick);
+    const TickDescription newState = mSimulator->Tick();
+    ASSERT_EQ(newState.mPowerUps.size(), 0);
+    ASSERT_EQ(newState.mMe.mRunningShoesTick, 22);
+}
+
+TEST_F(SimulateTest, PowerupPickupShoeAddition)
+{
+    // clang-format off
+    std::vector<std::string> info = {
+        "REQ 775 0 1",
+        "VAMPIRE 1 1 1 1 1 2 7",
+        "POWERUP SHOE 3 1 1",
+    };
+    // clang-format on
+    const TickDescription tick = solver::parseTickDescription(info);
+    mSimulator->SetState(tick);
+    const TickDescription newState = mSimulator->Tick();
+    ASSERT_EQ(newState.mPowerUps.size(), 0);
+    ASSERT_EQ(newState.mMe.mRunningShoesTick, 28);
+}
+
+// TEST_F(SimulateTest, NONONONONOOOOOOO)
+// {
+//     // clang-format off
+//     std::vector<std::string> info = {
+//         "REQ 775 0 1",
+//         "VAMPIRE 1 1 1 3 1 2 0",
+//         "VAMPIRE 3 9 9 3 1 2 0",
+//         "VAMPIRE 4 1 9 3 1 2 0",
+//         "VAMPIRE 2 9 1 3 1 2 0",
+//         "BAT1 4 1 5 1 6 1 3 2 7 2 2 3 3 3 7 3 8 3 1 4 9 4 1 5 9 5 1 6 9 6 2 7 3 7 7 7 8 7 3 8 7 8 4 9 5 9 6 9",
+//         "BAT2 5 2 4 3 6 3 3 4 7 4 2 5 8 5 3 6 7 6 4 7 6 7 5 8",
+//         "BAT3 5 3 5 4 3 5 4 5 5 5 6 5 7 5 5 6 5 7",
+//         "GRENADE 1 1 3 15 2",
+//         "GRENADE 1 9 3 0 2",
+//         "POWERUP TOMATO -3 2 1",
+//         "POWERUP GRENADE 2 3 1",
+//         "POWERUP BATTERY 10 7 1",
+//         "POWERUP SHOE -10 8 1"
+//     };
+//     // clang-format on
+//     const TickDescription tick = solver::parseTickDescription(info);
+//     mSimulator->SetState(tick);
+// }

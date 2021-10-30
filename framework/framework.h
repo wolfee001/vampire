9995f7e408aeed1e4d84c6db745527aa7e4cb9c7
wolfee <wolfee.dm@gmail.com>
2021-10-31 01:43:57 +0200

    dummy run button :)

--- framework/framework.h
+++ framework/framework.h
@@ -28,7 +28,7 @@ private:
     TickDescription mTickDescription;
 
     int mMapSelector = 0;
-    bool mRecordGame = true;
+    bool mRecordGame = false;
 
     std::map<std::string, void*> mAssets;
 

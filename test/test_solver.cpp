c5117b743451fbc01b0b7ce57b5070cb2774b517
wolfee <wolfee.dm@gmail.com>
2021-11-03 21:04:43 +0100

    fix yx test

--- test/test_solver.cpp
+++ test/test_solver.cpp
@@ -25,7 +25,7 @@ TEST(Parsing, ParseGameStart)
     EXPECT_EQ(description.mMapSize, 11);
 }
 
-TEST(DISABLED_Parsing, ParseTick)
+TEST(Parsing, ParseTick)
 {
     std::vector<std::string> info;
     TickDescription desc;
@@ -34,8 +34,8 @@ TEST(DISABLED_Parsing, ParseTick)
         "REQ 775 0 1",
         "VAMPIRE 1 1 1 3 1 2 0",
         "VAMPIRE 3 9 9 3 1 2 0",
-        "VAMPIRE 4 1 9 3 1 2 0",
-        "VAMPIRE 2 9 1 3 1 2 0",
+        "VAMPIRE 4 9 1 3 1 2 0",
+        "VAMPIRE 2 1 9 3 1 2 0",
         "BAT1 4 1 5 1 6 1 3 2 7 2 2 3 3 3 7 3 8 3 1 4 9 4 1 5 9 5 1 6 9 6 2 7 3 7 7 7 8 7 3 8 7 8 4 9 5 9 6 9",
         "BAT2 5 2 4 3 6 3 3 4 7 4 2 5 8 5 3 6 7 6 4 7 6 7 5 8",
         "BAT3 5 3 5 4 3 5 4 5 5 5 6 5 7 5 5 6 5 7",
@@ -95,9 +95,9 @@ TEST(DISABLED_Parsing, ParseTick)
 
     // clang-format off
     info = { 
-        "BAT1 4 1 5 1",
-        "BAT2 5 2 4 3",
-        "BAT3 5 3 5 4",
+        "BAT1 1 4 1 5",
+        "BAT2 2 5 3 4",
+        "BAT3 3 5 4 5",
     };
     // clang-format on
     desc = parseTickDescription(info);
@@ -112,7 +112,7 @@ TEST(DISABLED_Parsing, ParseTick)
 
     // clang-format off
     info = { 
-        "GRENADE 1 1 3 15 2",
+        "GRENADE 1 3 1 15 2",
         "GRENADE 1 9 3 0 2",
     };
     // clang-format on
@@ -126,7 +126,7 @@ TEST(DISABLED_Parsing, ParseTick)
 
     // clang-format off
     info = { 
-        "POWERUP TOMATO -3 2 1",
+        "POWERUP TOMATO -3 1 2",
         "POWERUP GRENADE 2 3 1",
         "POWERUP BATTERY 10 7 1",
         "POWERUP SHOE -10 8 1"
